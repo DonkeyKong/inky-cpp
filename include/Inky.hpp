f72496c5b8356d91a4a4c6804a96816b95dbdedf
DonkeyKong <admdonkeykong@gmail.com>
2023-01-22 23:26:26 -0500

    Add inky eeprom read

--- include/Inky.hpp
+++ include/Inky.hpp
@@ -1,13 +1,65 @@
 #pragma once
 
 #include "Image.hpp"
+#include "I2CDevice.hpp"
 
-class Inky
+class Inky : public I2CDevice
 {
 public:
+
+  enum class ColorCapability : uint8_t
+  {
+    //Invalid = 0,
+    BlackWhite = 1,
+    BlackWhiteRed = 2,
+    BlackWhiteYellow = 3,
+    //Invalid = 4,
+    SevenColor = 5
+  };
+
+  enum class DisplayVariant : uint8_t
+  {
+      //Invalid = 0,
+      Red_pHAT_High_Temp = 1,
+      Yellow_wHAT = 2,
+      Black_wHAT = 3,
+      Black_pHAT = 4,
+      Yellow_pHAT = 5,
+      Red_wHAT = 6,
+      Red_wHAT_High_Temp = 7,
+      Red_wHAT_v2 = 8,
+      //Invalid = 9,
+      Black_pHAT_SSD1608 = 10,
+      Red_pHAT_SSD1608 = 11,
+      Yellow_pHAT_SSD1608 = 12,
+      //Invalid = 13,
+      Seven_Colour_UC8159 = 14,
+      Seven_Colour_640x400_UC8159 = 15,
+      Seven_Colour_640x400_UC8159_v2 = 16,
+      Black_wHAT_SSD1683 = 17,
+      Red_wHAT_SSD1683 = 18,
+      Yellow_wHAT_SSD1683 = 19
+  };
+
   Inky();
   ~Inky();
   void setImage(Image image);
   void setBorder(InkyColor color);
   void show();
+
+  uint16_t width() const;
+  uint16_t height() const;
+  ColorCapability colorCapability() const;
+  uint8_t pcbVariant() const;
+  DisplayVariant displayVariant() const;
+  std::string writeTime() const;
+
+private:
+  uint16_t width_;
+  uint16_t height_;
+  ColorCapability colorCapability_;
+  uint8_t pcbVariant_;
+  DisplayVariant displayVariant_;
+  std::string writeTime_;
+  void readEeprom();
 };
--- src/Inky.cpp
+++ src/Inky.cpp
@@ -1,14 +1,76 @@
 #include "Inky.hpp"
+#ifdef PI_HOST
+#include "minimal_gpio.h"
+#endif
+#include <algorithm>
 
+static const uint8_t InkyEEPROMI2CDeviceId = 0x50;
 
-Inky::Inky()
+Inky::Inky() : I2CDevice(InkyEEPROMI2CDeviceId)
 {
-
+  readEeprom();
 }
 
 Inky::~Inky()
 {
 
+}
+
+static uint16_t read16(uint8_t* buf)
+{
+  return ((uint16_t)buf[0]) | ((uint16_t)buf[1] << 8);
+}
+
+void Inky::readEeprom()
+{
+  // eeprom format is python struct '<HHBBB22p'
+  // width, height, color, pcb_variant, display_variant, write_time
+  uint8_t rom[30];
+  readI2C(0, rom, 29);
+
+  width_ = read16(rom);
+  height_ = read16(rom+2);
+  colorCapability_ = (ColorCapability)rom[4];
+  pcbVariant_ = rom[5];
+  displayVariant_ = (DisplayVariant)rom[6];
+
+  // Get the length of the string and null terminate it
+  // The data could lie about length, so cap it to 21
+  uint8_t len = std::min(rom[7],(uint8_t)21); 
+  rom[8+len] = '\0';
+
+  // Consruct a std::string out of our newly minted cstr
+  writeTime_ = std::string((const char*)rom+8);
+}
+
+uint16_t Inky::width() const 
+{
+  return width_;
+}
+
+uint16_t Inky::height() const
+{
+  return height_;
+}
+
+Inky::ColorCapability Inky::colorCapability() const
+{
+  return colorCapability_;
+}
+
+uint8_t Inky::pcbVariant() const
+{
+  return pcbVariant_;
+}
+
+Inky::DisplayVariant Inky::displayVariant() const
+{
+  return displayVariant_;
+}
+
+std::string Inky::writeTime() const
+{
+  return writeTime_;
 }
 
 void Inky::setImage(Image image)
